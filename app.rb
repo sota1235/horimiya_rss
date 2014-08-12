ec775f4dafabceca20b30e70ab0ebef6197ad716
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-08-13 01:45:45 +0900

    fix some files / delete haml

--- app.rb
+++ app.rb
@@ -1,12 +1,11 @@
 require 'sinatra'
 require 'sinatra/base'
-require 'haml'
 
 require_relative 'models/init'
 
 class Server < Sinatra::Base
   get '/' do
+    content_type 'text/xml'
     @body = make_rss
-    haml :index
   end
 end
